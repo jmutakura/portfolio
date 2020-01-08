94dc9d8af7d47103fb42ce366a080c1bf2e4f14d
Jonathan Mutakura <jmutakura@gmail.com>
2020-01-08 17:27:05 -0600

    Done the following:   * Made the contact page and gave it functionality   * Made the redirection page for after submitting the form   * Updated the favicon   * Gave the contact form actions and methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -11,6 +11,16 @@ def home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'Something went wrong'
 
 
 if __name__ == '__main__':
